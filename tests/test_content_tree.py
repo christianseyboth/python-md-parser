9144a21a8d8909e214bf864031488564240375f9
christianseyboth <cseyboth@mailbox.org>
2026-03-11 08:15:36 +0100

    added succesful content_tree() tests

--- tests/test_content_tree.py
+++ tests/test_content_tree.py
@@ -12,10 +12,9 @@ def test_content_tree_return_type():
 
 def test_content_tree_frontmatter():
     result = create_content_tree("tests/fixtures/valid")
-    tier = asdict(result[0])
 
-    assert tier["tier"] == 1
-    assert tier["title"] == "tier-1-interface"
-    assert tier["chapters"][0]["id"] == "terminal-basics"
-    assert tier["chapters"][0]["lessons"][0]["title"] == "Your First Command"
-    assert tier["chapters"][0]["lessons"][1]["title"] == "List file system"
+    assert result[0].tier == 1
+    assert result[0].title == "tier-1-interface"
+    assert result[0].chapters[0].id == "terminal-basics"
+    assert result[0].chapters[0].lessons[0].title == "Your First Command"
+    assert result[0].chapters[0].lessons[1].title == "List file system"
